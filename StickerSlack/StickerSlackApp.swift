cf196bc5ec3a41eb6ee2073ad9dafe2f202be414
neon443 <69979447+neon443@users.noreply.github.com>
2025-10-15 22:39:26 +0100

    Initial Commit

---
+++ StickerSlack/StickerSlackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerSlackApp.swift
+//  StickerSlack
+//
+//  Created by neon443 on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StickerSlackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
