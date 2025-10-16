8e3608249090c4963809a931e7e25ef8a2e13a1f
neon443 <69979447+neon443@users.noreply.github.com>
2025-10-16 17:59:41 +0100

    added testdata

--- StickerSlack/Views/ContentView.swift
+++ StickerSlack/Views/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		TabView {
+			Text("hi")
+				.tabItem {
+					Label("home", systemImage: "house")
+				}
+		}
     }
 }
 
