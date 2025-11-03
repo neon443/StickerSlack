4c9cd6c8e61ff18ef036f128b47645e4de21d424
neon443 <69979447+neon443@users.noreply.github.com>
2025-11-03 20:40:27 +0000

    use emojirow in trietestingview extract trietestingview

---
+++ StickerSlack/Emoji/TrieTestingView.swift
@@ -0,0 +1,81 @@
+//
+//  TrieTestingView.swift
+//  StickerSlack
+//
+//  Created by neon443 on 03/11/2025.
+//
+
+import SwiftUI
+
+struct TrieTestingView: View {
+	@ObservedObject var hoarder: EmojiHoarder = EmojiHoarder(localOnly: true)
+	
+	@State var id: UUID = UUID()
+	
+	@State var newWord: String = "hello"
+	
+	@State var searchTerm: String = ""
+	@State var searchStatus: Bool? = nil
+	
+	@State var filterTerm: String = ""
+	@State var filterResult: [String] = []
+	
+	var body: some View {
+		VStack {
+			Button("reset", role: .destructive) {
+				hoarder.trie.root = TrieNode()
+			}
+			Button("add emojis!") {
+				hoarder.buildTrie()
+			}
+			.buttonStyle(.borderedProminent)
+			
+			TextField("", text: $searchTerm)
+				.textFieldStyle(.roundedBorder)
+				.border(.orange)
+				.onChange(of: searchTerm) { _ in
+					searchStatus = hoarder.trie.search(for: searchTerm)
+				}
+			if let searchStatus {
+				Circle()
+					.frame(width: 20, height: 20)
+					.foregroundStyle(searchStatus ? .green : .red)
+			}
+			
+			TextField("", text: $filterTerm)
+				.textFieldStyle(.roundedBorder)
+				.border(.orange)
+				.onChange(of: filterTerm) { _ in
+					withAnimation { filterResult = hoarder.trie.search(prefix: filterTerm) }
+				}
+			Text("\(filterResult.count)")
+				.modifier(numericTextCompat())
+			
+			List(filterResult, id: \.self) { item in
+				EmojiRow(hoarder: hoarder, emoji: hoarder.trie.dict[item]!)
+			}
+			
+			Text("\(hoarder.trie.root.children.count)")
+		}
+	}
+}
+
+struct TrieNodeView: View {
+	@ObservedObject var trie: Trie
+	@State var trieNode: TrieNode
+	
+	var body: some View {
+		ForEach(trieNode.children.map { $0.key }, id: \.self) { key in
+			let node = trieNode.children[key]!
+			Text(String(key))
+				.foregroundStyle(node.isEndOfWord ? .red : .primary)
+				.frame(width: 20, height: 20)
+			TrieNodeView(trie: trie, trieNode: node)
+				.padding(.leading, 20)
+		}
+	}
+}
+
+#Preview {
+	TrieTestingView()
+}
